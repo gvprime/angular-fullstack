a36e2132984da6ba79bdf1a463e8f789384a29af
Rémi Castaing <remi.castaing@gmail.com>
2014-08-26 22:03:27 +0200

    initialize canary

---
+++ client/app/account/account.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module('angularFullstackApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('login', {
+        url: '/login',
+        templateUrl: 'app/account/login/login.html',
+        controller: 'LoginCtrl'
+      })
+      .state('loginWithToken', {
+        url: '/login/:sessionToken',
+        template: ' ',
+        controller: function($stateParams, Auth, $location){
+          if ($stateParams.sessionToken) {
+            Auth.setSessionToken($stateParams.sessionToken, function(){$location.path('/');});
+          }
+        }
+      })
+      .state('signup', {
+        url: '/signup',
+        templateUrl: 'app/account/signup/signup.html',
+        controller: 'SignupCtrl'
+      })
+      .state('settings', {
+        url: '/settings',
+        templateUrl: 'app/account/settings/settings.html',
+        controller: 'SettingsCtrl',
+        authenticate: true
+      });
+  });
